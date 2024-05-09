568df6f792e7b709bf9a507498c0cb23300379e0
Dinesh Kumar E <egdedk@gmail.com>
2024-05-10 01:26:41 +0530

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useDeferredValue, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const input = useRef('');
-  const [suggestions, setSuggestions] = useState('');
+  const [input, setInput] = useState("");
+  const [suggestions, setSuggestions] = useState("");
 
   const customDictionary = {
     teh: "the",
@@ -14,36 +14,49 @@ function App() {
     exampl: "example",
   };
 
-  const handleChange = (e)=>{
-    const {value} = e.target;
-    input.current= input.current+value;
-      const words = input.current.split(' ');
-      const correctedwords = words.map((item) => {
-      const word = customDictionary[item.toLocaleLowerCase()] ;
-      return word || item ;
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  function suggestion(input) {
+    const words = input.split(" ");
+    const correctedwords = words.map((item) => {
+      const word = customDictionary[item.toLocaleLowerCase()];
+      return word || item;
     });
-    let correctedText = correctedwords.join(' ');
-    console.log(correctedwords,'correctedwords')
-    console.log(words,'words')
-    const firstCorrection = correctedwords.find((word,idx)=>{
-      return word!==(words[idx]);
+    let correctedText = correctedwords.join(" ");
+    console.log(correctedwords, "correctedwords");
+    console.log(words, "words");
+    const firstCorrection = correctedwords.find((word, idx) => {
+      return word !== words[idx];
     });
 
-    setSuggestions(firstCorrection || '') ;
+    setSuggestions(firstCorrection || "");
   }
 
-  
-
-
+  useEffect(() => {
+    suggestion(input);
+  },[input])
 
   return (
     <>
       <h2>Spell Check and Auto-Correction</h2>
-      <textarea name="textarea" placeholder="Enter text..." value={input.current} onChange={handleChange}></textarea>
-      {suggestions ? <div>Did you mean: <strong>{suggestions}?</strong></div> : ''}
+      <textarea
+        name="textarea"
+        placeholder="Enter text..."
+        value={input}
+        onChange={handleChange}
+      ></textarea>
+      {suggestions ? (
+        <div>
+          Did you mean: <strong>{suggestions}?</strong>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
 
 export default App;
-
